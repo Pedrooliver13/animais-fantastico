c109bd04269de69cf10255b0149aa3800d6b9116
Pedro <you@example.com>
2020-10-06 10:33:23 -0300

    navtab refatorado

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,14 @@
 import initMenuDropDown from "./module/menu-dropdown.js";
 import initMenuMobile from "./module/menu-mobile.js";
 import SmoothScroll from "./module/smooth-scroll.js";
-import initNavTab from "./module/nav-tab.js";
+import NavTab from "./module/nav-tab.js";
 import Modal from "./module/modal.js";
 import initTooltip from "./module/tooltip.js";
-import initWorkOperation from './module/work-operation.js';
-import initAnimalFetch from './module/animais-fetch.js';
-import initBitcoinFetch from './module/bitcoin-fetch.js';
-import Accordion from './module/accordion-list.js';
-import AnimaScroll from './module/anima-scroll.js';
+import initWorkOperation from "./module/work-operation.js";
+import initAnimalFetch from "./module/animais-fetch.js";
+import initBitcoinFetch from "./module/bitcoin-fetch.js";
+import Accordion from "./module/accordion-list.js";
+import AnimaScroll from "./module/anima-scroll.js";
 // import Countdown from './module/countdown.js';
 // import initExercicio from './module/exercicio.js';
 
@@ -21,12 +21,18 @@ animaScroll.init();
 const accordion = new Accordion('[data-menu="accordion"] dt');
 accordion.init();
 
-const modal = new Modal('[data-modal="open"]', '[data-modal="close"]', '[data-modal="container"]');
+const modal = new Modal(
+  '[data-modal="open"]',
+  '[data-modal="close"]',
+  '[data-modal="container"]'
+);
 modal.init();
+
+const navTab = new NavTab(".js-animais li", "[data-tab='content'] section");
+navTab.init();
 
 initMenuDropDown();
 initMenuMobile();
-initNavTab();
 initTooltip(); // tooltip => modalzinho quando passa o mouse por cima
 initWorkOperation();
 initAnimalFetch();
@@ -37,8 +43,5 @@ initBitcoinFetch();
 // setInterval(()=> {
 //   console.log(timeOfChrismas.total)
 // }, 1000)
-
-
-
 
 // initExercicio();
