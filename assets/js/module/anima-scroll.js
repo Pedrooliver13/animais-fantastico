3266244aa330616340bcd271002df4210834cf9f
Pedro <you@example.com>
2020-10-05 22:52:02 -0300

    refeito anima-scroll

--- assets/js/module/anima-scroll.js
+++ assets/js/module/anima-scroll.js
@@ -1,9 +1,11 @@
+import debouce from './debounce.js';
+
 export default class AnimaScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.checkDistance = this.checkDistance.bind(this);
+    this.checkDistance = debouce(this.checkDistance.bind(this), 50);
   }
 
   getDistance() {
