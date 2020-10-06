31701685be173aadeb036819d0b8b52b2901f2be
Pedro <you@example.com>
2020-10-06 15:46:19 -0300

    refatorando Work Operation

--- assets/js/module/menu-mobile.js
+++ assets/js/module/menu-mobile.js
@@ -1,9 +1,15 @@
 import outsideClick from "./outside-click.js";
 
 export default class MenuMobile {
-  constructor(button, menu) {
+  constructor(button, menu, activeClass) {
     this.menuButton = document.querySelector(button);
     this.menuList = document.querySelector(menu);
+
+    if (activeClass === undefined) {
+      this.activeClass = "ativo";
+    } else {
+      this.activeClass = activeClass;
+    }
 
     this.activeMenu = this.activeMenu.bind(this);
   }
@@ -12,21 +18,21 @@ export default class MenuMobile {
     this.menuButton.classList.add("ativo");
     this.menuList.classList.add("ativo");
 
-    outsideClick(this.menuList, ['click'], () => {
+    outsideClick(this.menuList, ["click"], () => {
       this.menuButton.classList.remove("ativo");
       this.menuList.classList.remove("ativo");
     });
   }
 
   addMenuMobileEvent() {
-    this.menuButton.addEventListener('click', this.activeMenu);
+    this.menuButton.addEventListener("click", this.activeMenu);
   }
 
   init() {
-    if(this.menuButton && this.menuList) {
+    if (this.menuButton && this.menuList) {
       this.addMenuMobileEvent();
     }
 
-    return this; 
+    return this;
   }
 }
